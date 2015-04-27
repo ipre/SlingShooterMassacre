d290098d75b5ac661bf53ec87fda313cd6b609e3
Eric Heung <eric.heung@live.com>
2015-04-27 13:28:29 +0200

    GameDev2 #3

---
+++ Assets/Scripts/Slingshot.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Slingshot : MonoBehaviour {
+
+	private GameObject launchPoint;
+
+	void Awake(){
+		Transform launchPointTrans = transform.Find ("Launchpoint");
+		launchPoint = launchPointTrans.gameObject;
+		launchPoint.SetActive (false);
+	}
+
+	void OnMouseEnter(){
+		print ("Slingshot:Enter");	
+		launchPoint.SetActive (true);
+	//	launchPoint.transform.position = Input.mousePosition
+	}
+
+	void OnMouseExit(){
+		print ("Slingshot:Exit");
+		launchPoint.SetActive (false);
+	}
+}
